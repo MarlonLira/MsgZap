296d40a52ca2d40dfd628262a104d5168d9b4627
Marlon Lira <marlonlira3@gmail.com>
2018-11-21 19:52:56 -0300

    Sistema de envio de mensagem de forma massiva para o WhatApp

---
+++ MsgZap/Helper/Hlp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MsgZap.Helper
+{
+    class Hlp
+    {
+
+        public String RandomIdGenerator()
+        {
+            String Id = "";
+            String Rd = "";
+
+            try
+            {
+                Rd = Guid.NewGuid().ToString();
+
+                Id = "msg-" + RandomNumber(0, 99) + "-" + Rd;
+
+                int RandomNumber(int min, int max)
+                {
+                    Random random = new Random();
+                    return random.Next(min, max);
+                }
+
+                return Id;
+            }
+            finally
+            {
+                Id = "";
+                Rd = "";
+            }
+
+
+            
+        }
+    }
+}
---
+++ MsgZap/Model/WhatsAppSend.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MsgZap.Model
+{
+    class WhatsAppSend
+    {
+
+        public String Remetente { get; set; }
+        public String Destinatario { get; set; }
+        public String Id { get; set; }
+        public String Cmd { get; set; }
+        public String Token { get; set; }
+        public String Texto { get; set; }
+
+    }
+}
